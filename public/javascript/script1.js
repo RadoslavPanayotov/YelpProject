6b749720f23af7b3cb1bc40cc3914c7a6b8557b4
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-24 16:13:08 +0300

    commit

--- public/javascript/script1.js
+++ public/javascript/script1.js
@@ -4,22 +4,19 @@ app.controller('login', function ($scope, $http) {
         // $http.get('https://projectxnr.herokuapp.com/users').then(function (response) {
         //    console.log(response.data);
         // });
-        // $http({
-        //     // 'https://projectxnr.herokuapp.com/users', x
-        //     url: 'https://projectxnr.herokuapp.com/users',
-        //     method: "POST",
-        //     data: $scope.user,
-        //     withCredentials: true,
-        //     headers: {
-        //         'Content-Type': 'application/json; charset=utf-8'
-        //     }
-        // });
-        console.log($scope.user.password + " " + $scope.user.username);
-        var x = JSON.stringify($scope.user);
-        $http.post('https://projectxnr.herokuapp.com/users', x).then(function (response) {
-            console.log(response.data);
-        }, (function errorCallback(response) {
-            console.log("Error" + response.data);
-        }));
+        $http({
+            url: 'https://projectxnr.herokuapp.com/users',
+            method: "POST",
+            data: $scope.user,
+            withCredentials: true,
+            headers:  {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+        // console.log($scope.user.password + " " + $scope.user.username);
+        // var x = JSON.stringify($scope.user);
+        // $http.post('https://projectxnr.herokuapp.com/users', x).then(function (response) {
+        //     console.log(response.data);
+        // }, (function errorCallback(response) {
+        //     console.log("Error" + response.data);
+        // }));
     }
 });
