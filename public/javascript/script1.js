59749f6aed1c37948cd1522b6d282228368fa48d
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-24 15:43:26 +0300

    commited again

--- public/javascript/script1.js
+++ public/javascript/script1.js
@@ -1,13 +1,26 @@
 var app = angular.module('myApp', []);
 app.controller('login', function ($scope, $http) {
-    $scope.logMe= function () {
+    $scope.logMe = function () {
+        // $http.get('https://projectxnr.herokuapp.com/users').then(function (response) {
+        //    console.log(response.data);
+        // });
         console.log($scope.user.password + " " + $scope.user.username);
         var x = JSON.stringify($scope.user);
-        $http.post('https://projectxnr.herokuapp.com/users', x).then(function (response) {
-           console.log(response.data);
-        }), (function errorCallback(response) {
-            console.log("Error" + response.data);
+        $http({
+            // 'https://projectxnr.herokuapp.com/users', x
+            url: 'https://projectxnr.herokuapp.com/users',
+            method: "POST",
+            data: $scope.user,
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
         });
+        //     }).then(function (response) {
+        //         console.log(response.data);
+        //     }), (function errorCallback(response) {
+        //         console.log("Error" + response.data);
+        //     });
+        // }
     }
-
 });
