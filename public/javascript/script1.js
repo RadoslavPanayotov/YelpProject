fd6f5755047c810e0bd27801d653ad890e527000
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-24 14:25:40 +0300

    commited again

--- public/javascript/script1.js
+++ public/javascript/script1.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 app.controller('login', function ($scope, $http) {
     $scope.logMe= function () {
         console.log($scope.user.password + " " + $scope.user.username);
-        $http.get('https://projectxnr.herokuapp.com/users', $scope.user).then(function (response) {
+        $http.post('https://projectxnr.herokuapp.com/users', $scope.user).then(function (response) {
            console.log(response.data);
         }), (function errorCallback(response) {
             console.log("Error" + response.data);
