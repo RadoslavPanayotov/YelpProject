84dce0209dee1709bdfb769c4797f08f32e4db2c
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-24 15:11:34 +0300

    changes were made

--- public/javascript/script1.js
+++ public/javascript/script1.js
@@ -2,7 +2,8 @@ var app = angular.module('myApp', []);
 app.controller('login', function ($scope, $http) {
     $scope.logMe= function () {
         console.log($scope.user.password + " " + $scope.user.username);
-        $http.post('https://projectxnr.herokuapp.com/users', $scope.user).then(function (response) {
+        var x = JSON.stringify($scope.user);
+        $http.post('https://projectxnr.herokuapp.com/users', x).then(function (response) {
            console.log(response.data);
         }), (function errorCallback(response) {
             console.log("Error" + response.data);
