475a21c3ab7cba235aa0a9d243fb14d0ac47e088
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-24 15:57:35 +0300

    commits were made

--- public/javascript/script1.js
+++ public/javascript/script1.js
@@ -1,21 +1,21 @@
 var app = angular.module('myApp', []);
 app.controller('login', function ($scope, $http) {
     $scope.logMe = function () {
-        $http.get('https://projectxnr.herokuapp.com/users').then(function (response) {
-           console.log(response.data);
-        });
+        // $http.get('https://projectxnr.herokuapp.com/users').then(function (response) {
+        //    console.log(response.data);
+        // });
         // console.log($scope.user.password + " " + $scope.user.username);
         // var x = JSON.stringify($scope.user);
-        // $http({
-        //     // 'https://projectxnr.herokuapp.com/users', x
-        //     url: 'https://projectxnr.herokuapp.com/users',
-        //     method: "GET",
-        //     data: $scope.user,
-        //     withCredentials: true,
-        //     headers: {
-        //         'Content-Type': 'application/json; charset=utf-8'
-        //     }
-        // });
+        $http({
+            // 'https://projectxnr.herokuapp.com/users', x
+            url: 'https://projectxnr.herokuapp.com/users',
+            method: "GET",
+            data: $scope.user,
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        });
         // //     }).then(function (response) {
         // //         console.log(response.data);
         // //     }), (function errorCallback(response) {
