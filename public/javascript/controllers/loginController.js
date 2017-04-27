a463a3d1931d59f7cca62f916e75bbaee30c7b2d
Nikolay Milenkov <nikolito007@Nikolays-MacBook-Air.local>
2017-04-27 12:42:06 +0300

    ok

--- public/javascript/controllers/loginController.js
+++ public/javascript/controllers/loginController.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-app.controller('login', function($scope, userService, $rootScope) {
-    $scope.errorMessage = '';
 
-    $scope.logMe = function() {
-        // $http({
-        //     url: 'http://localhost:3000/users',
-        //     method: "POST",
-        //     data: JSON.stringify($scope.user),
-        //     withCredentials: true,
-
-        // })
-        console.log(userService);
-        userService.postReq('http://localhost:3000/users', 'POST', JSON.stringify($scope.user)).then(function(data) {
-            console.log(data);
-            if (data.data.value == "true") {
-=======
 app.controller('login', function ($scope, userService, $rootScope) {
    $scope.errorMessage = '';
    
@@ -22,7 +6,6 @@ app.controller('login', function ($scope, userService, $rootScope) {
         userService.postReq('http://localhost:3000/users','POST',JSON.stringify($scope.user)).then(function (data){    
             console.log(data);
             if(data.data.value == "true"){ 
->>>>>>> 7a84955b5103b3f1ca1c743ebe0457c61e384f60
                 $rootScope.isLogged = false;
             } else {
                 $scope.errorMessage = "Username or password are invalid!"
