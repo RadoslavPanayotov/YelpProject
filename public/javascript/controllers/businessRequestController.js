81d9d29db9a1ef000baf89a3b93797b5d2ff80ee
Nikolay Milenkov <nikolito007@Nikolays-MacBook-Air.local>
2017-04-30 14:05:23 +0300

    enter

--- public/javascript/controllers/businessRequestController.js
+++ public/javascript/controllers/businessRequestController.js
@@ -1,7 +1,10 @@
-app.controller('bus', function ($scope, isLoggedService, userService, $rootScope, $location, $window) {
-  var data = {value: 'Divaka'}
-  userService.postReq('http://localhost:3000/businesses', 'POST', data).then(function (data) {
-    console.log(data)
-  })
+app.controller('bus', function($scope, isLoggedService, userService, $rootScope, $location, $window) {
+    $scope.searchMe = function() {
+        var data = {value: $scope.searchResult};
+        console.log(data);
+        userService.postReq('http://localhost:3000/businesses', 'POST', data).then(function(data) {
+            console.log(data);
+        });
+    }
 
 })
