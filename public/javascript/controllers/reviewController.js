65ba011b83fa9e18681a6b203a26c589a930f9ea
Nikolay Milenkov <nikolito007@Nikolays-MacBook-Air.local>
2017-05-02 07:29:07 +0300

    commited

--- public/javascript/controllers/reviewController.js
+++ public/javascript/controllers/reviewController.js
@@ -1,24 +1,26 @@
 app.controller('reviewsController', function ($scope, $rootScope, userService, $window, $location) {
-     console.log($scope.businessItem);
-  // var logedUser = $window.sessionStorage.getItem('userId')
-  // if (logedUser == undefined) {
-  //   $location.path('/map')
-  // }
+   
+   var logedUser = $window.sessionStorage.getItem('userId')
+  if (logedUser == undefined) {
+    $location.path('/map')
+  }
   $scope.reviewForWriting = false
   $scope.businessItem = $rootScope.clickedItem
   $scope.writeAReview = function () {
     console.log('sasa')
-    $scope.reviewForWriting = true
+    $scope.reviewForWriting = true;
   }
   $scope.reviewError = '';
   $scope.textarea = '';
   $scope.submitReview = function () {
-    // if($scope.textarea == ''){
-    //   $scope.reviewError = "You can't send an empty review!";
-    //   return;
-    // }
-    //userService.postReq("")
- 
+    var data = {objectId: $scope.businessItem._id, userId:logedUser,collection: $scope.businessItem.type, text: $scope.textarea};
+    if($scope.textarea == ''){
+      $scope.reviewError = "You can't send an empty review!";
+      return;
+    }
+    userService.postReq("http://localhost:3000/reviews","POST",data).then(function(data){
+      console.log(data);
+    });
   }
   
  
