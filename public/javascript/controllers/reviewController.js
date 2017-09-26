e7c914286742e562f2fbd2e6d1cf46f3997608a9
Nikolay Milenkov <nikolaymilenkov07@gmail.com>
2017-09-26 22:17:15 +0300

    score

--- public/javascript/controllers/reviewController.js
+++ public/javascript/controllers/reviewController.js
@@ -1,23 +1,46 @@
-app.controller('reviewsController', function ($scope, $rootScope, userService, $window, $location) {
-  var logedUser = $window.sessionStorage.getItem('userId')
-  if (logedUser == undefined) {
-    $location.path('/map');
-  }
-  $scope.reviewForWriting = false;
-  $scope.businessItem = $rootScope.clickedItem;
-  $scope.writeAReview = function () {
-    $scope.reviewForWriting = true;
-  }
-  $scope.reviewError = '';
-  $scope.textarea = '';
-  $scope.submitReview = function () {
-    var data = {objectId: $scope.businessItem._id, userId: logedUser,collection: $scope.businessItem.type, text: $scope.textarea}
-    if ($scope.textarea == '') {
-      $scope.reviewError = "You can't send an empty review!";
-      return
+app.controller('reviewsController', function($scope, $rootScope, userService, $window, $location) {
+    var logedUser = $window.sessionStorage.getItem('userId')
+    if (logedUser == undefined) {
+        $location.path('/map');
     }
-    userService.postReq('https://projectxnr.herokuapp.com/reviews', 'POST', data).then(function (data) {
-      $scope.reviewForWriting = false;
-    });
-  }
+    $scope.reviewForWriting = false;
+    $scope.businessItem = $rootScope.clickedItem;
+    $scope.writeAReview = function() {
+        $scope.reviewForWriting = true;
+    }
+    $scope.reviewError = '';
+    $scope.textarea = '';
+    $scope.writeAScore = function() {
+        $scope.raitingForWriting = true;
+    }
+    $scope.minScore = 0;
+    $scope.maxScore = 5;
+    $scope.submitReview = function() {
+        var data = { objectId: $scope.businessItem._id, userId: logedUser, collection: $scope.businessItem.type, text: $scope.textarea }
+        if ($scope.textarea == '') {
+            $scope.reviewError = "You can't send an empty review!";
+            return
+        }
+        userService.postReq('https://projectxnr.herokuapp.com/reviews', 'POST', data).then(function(data) {
+            $scope.reviewForWriting = false;
+        });
+    }
+    $scope.submitScore = function() {
+
+        var data = {
+            objectId: $scope.businessItem._id,
+            score: $scope.score,
+            collection: $scope.businessItem.type
+        };
+        userService.postReq('http://projectxnr.herokuapp.com/score', 'POST', JSON.stringify(data)).then(function(data) {
+
+            $scope.reviewForWriting = false;
+            $scope.raitingForWriting = false;
+            if (data.data.value == 'true') {
+                alert('success');
+            } else {
+                alert('try again');
+            }
+        });
+    }
 });
